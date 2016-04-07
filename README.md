d152c97a98b715dbd98d0cd58e9f000938064151
katarzyna-z <katarzyna.zabrocka@intel.com>
2016-04-07 10:39:47 +0200

    initial implementation of exec plugin

--- README.md
+++ README.md
@@ -1,1 +1,215 @@
-# snap-plugin-collector-shell
+# snap plugin collector - exec
+
+The plugin launch executable files and collects their outputs.
+
+The plugin is a generic plugin. You can configure metrics names, type of collecting data, executables files, arguments necessary for executables files.
+
+This plugin gives powerful and dangerous tool - user can run any program. Running this plugin with root privileges may be extremely dangerous and it is not advised. 
+
+1. [Getting Started](#getting-started)
+  * [System Requirements](#system-requirements)
+  * [Operating systems](#operating-systems)
+  * [Installation](#installation)
+  * [Configuration and Usage](#configuration-and-usage)
+2. [Documentation](#documentation)
+  * [Collected Metrics](#collected-metrics)
+  * [snap's Global Config](#snaps-global-config)
+  * [Setfile structure](#setfile-structure)
+  * [Examples](#examples)
+  * [Roadmap](#roadmap)
+3. [Community Support](#community-support)
+4. [Contributing](#contributing)
+5. [License](#license)
+6. [Acknowledgements](#acknowledgements)
+
+## Getting Started
+
+### System Requirements
+
+* [golang 1.5+](https://golang.org/dl/) - needed only for building
+
+### Operating systems
+All OSs currently supported by snap:
+* Linux/amd64
+
+### Installation
+
+#### Download exec plugin binary:
+
+You can get the pre-built binaries for your OS and architecture at snap's [Github Releases](https://github.com/intelsdi-x/snap/releases) page.
+
+#### To build the plugin binary:
+
+Fork https://github.com/intelsdi-x/snap-plugin-collector-exec
+
+Clone repo into `$GOPATH/src/github/intelsdi-x/`:
+```
+$ git clone https://github.com/<yourGithubID>/snap-plugin-collector-exec
+```
+Build the plugin by running make in repo:
+```
+$ make
+```
+This builds the plugin in `/build/rootfs`.
+
+### Configuration and Usage
+
+* Set up the [snap framework](https://github.com/intelsdi-x/snap/blob/master/README.md#getting-started).
+
+* Create configuration file (called as a setfile) in which will be defined metrics (metrics names, type of collecting data, executables files, arguments necessary for executables files), read setfile structure description available in [setfile structure](https://github.com/intelsdi-x/snap-plugin-collector-exec/blob/master/README.md#setfile-structure) and see exemplary in [examples/setfiles/](https://github.com/intelsdi-x/snap-plugin-collector-exec/blob/master/examples/setfiles/).
+
+* Create Global Config, see description in [snap's Global Config] (https://github.com/intelsdi-x/snap-plugin-collector-exec/blob/master/README.md#snaps-global-config).
+ 
+Notice that this plugin is a generic plugin, it cannot work without configuration, because there is no reasonable default behavior. Errors in configuration or in execution of program/command block collecting of metrics, errors are logged but plugin still work.
+
+Setfile contains configuration which should be protected. It is advised to limit access for this configuration and regularly audit its content.
+
+It is not advised to use dynamic query notation in task manifest for exec plugin. User should consciously make decision on starting program or executing command to avoid unexpected changes.
+
+The executable file which is used to collect metrics must write its output (value of metric) to standard output (stdout) in plain form. User interaction cannot be needed by executable file. Some of commands or programs require special privileges to execute so user must check if configuration can be launch successfully.
+
+The executable file will be launch per interval set in task manifest and it may have impact on system performance. New process is started per interval for each of metric and this process must end and clean resources itself.
+
+## Documentation
+
+### Collected Metrics
+The plugin collects outputs of executable files.
+
+Metrics are available in namespace: `/intel/exec/<metric_name>/`.
+
+Metrics's names are defined in setfile.
+Metrics can be collected in one of following data types: float64, float32, int64, int32, int16, int8, uint64, uint32, uint16, uint8, string.
+
+### snap's Global Config
+Global configuration files are described in [snap's documentation](https://github.com/intelsdi-x/snap/blob/master/docs/SNAPD_CONFIGURATION.md). You have to add section "exec" in "collector" section and then specify following options:
+- `"setfile"` - path to exec plugin configuration file (path to setfile),
+- `"execution_timeout"` -   max time for command/program execution in seconds (default value: 10 sec).
+
+See example Global Config in [examples/cfg/] (https://github.com/intelsdi-x/snap-plugin-collector-exec/blob/master/examples/configs/).
+
+
+### Setfile structure
+
+Setfile contains JSON structure which is used to define metrics. 
+Metric is defined as JSON object in following format:
+```
+  "<metric_name>": {
+            "exec": "<executable_file>",
+            "type": "<data_type>",
+            "args": [ "<arg1>", "<arg1>", "<arg3>"]
+    }
+```
+where:
+- metric_name -  metric name which is used in metric's namespace (required),
+- executable_file -  path to executable file which should by launch to collect metric (required),
+- data_type -  metric data type (required)
+- arg1, arg1, arg3 -  arguments needed by executable file which is used to collect metric (optional).
+
+For example 'echo_metric' metric for  'echo' program which is available in '/bin' with argument '1.1' and  float64 data type should have following definition:
+```
+  "echo_metric": {
+            "exec": "/bin/echo",
+            "type": "float64",
+            "args": [ "1.1"]
+    }
+```
+The metric defined above has following namespace `/intel/exec/echo_metric`.
+
+If program returns metric with additional information then it is needed to use another tool to extract value of metric, see example below which shows extraction of numeric data from output of echo command:
+```
+"echo_metric": {
+				"exec": "/bin/sh",
+				"type": "int64",
+				"args": [ "-c", "echo  \"test:1775\" | awk -F':' '{printf $2}'"]
+		}
+```
+As it is shown, `exec` in setfile could be defined as combination of commands.
+
+### Examples
+Example running snap-plugin-collector-exec plugin and writing data to a file.
+
+Create configuration file (setfile) for exec plugin, see exemplary in [examples/setfiles/](https://github.com/intelsdi-x/snap-plugin-collector-exec/blob/master/examples/setfiles/).
+
+Set path to configuration file as the field `setfile` and maximal time for command/program execution as the field `execution_timeout` in Global Config, see exemplary in [examples/configs/] (https://github.com/intelsdi-x/snap-plugin-collector-exec/blob/master/examples/configs/).
+
+In one terminal window, open the snap daemon (in this case with logging set to 1,  trust disabled and global configuration saved in config.json ):
+```
+$ $SNAP_PATH/bin/snapd -l 1 -t 0 --config config.json
+```
+In another terminal window:
+
+Load snap-plugin-collector-exec plugin
+```
+$ $SNAP_PATH/bin/snapctl plugin load snap-plugin-collector-exec
+```
+Load file plugin for publishing:
+```
+$ $SNAP_PATH/bin/snapctl plugin load $SNAP_PATH/plugin/snap-publisher-file
+```
+See available metrics for your system
+
+```
+$ $SNAP_PATH/bin/snapctl metric list
+```
+
+Create a task manifest file to use snap-plugin-collector-exec plugin (exemplary in [examples/tasks/] (https://github.com/intelsdi-x/snap-plugin-collector-exec/blob/master/examples/tasks/)):
+```
+{
+{
+    "version": 1,
+    "schedule": {
+        "type": "simple",
+        "interval": "1s"
+    },
+    "workflow": {
+        "collect": {
+            "metrics": {
+                "/intel/exec/metric0": {},
+                "/intel/exec/metric1": {},
+                "/intel/exec/metric2": {},
+                "/intel/exec/metric3": {},
+                "/intel/exec/metric4": {}
+           },
+            "config": {
+            },
+            "process": null,
+            "publish": [
+                {
+                    "plugin_name": "file",
+                    "config": {
+                        "file": "/tmp/published_exec"
+                    }
+                }
+            ]
+        }
+    }
+}
+```
+
+Create a task:
+```
+$ $SNAP_PATH/bin/snapctl task create -t task.json
+```
+
+### Roadmap
+There isn't a current roadmap for this plugin, but it is in active development. As we launch this plugin, we do not have any outstanding requirements for the next release.
+
+## Community Support
+This repository is one of **many** plugins in **snap**, a powerful telemetry framework. See the full project at http://github.com/intelsdi-x/snap.
+
+To reach out on other use cases, visit:
+* [Snap Gitter channel](https://gitter.im/intelsdi-x/snap)
+
+## Contributing
+We love contributions!
+
+There's more than one way to give back, from examples to blogs to code updates. See our recommended process in [CONTRIBUTING.md](CONTRIBUTING.md).
+
+And **thank you!** Your contribution, through code and participation, is incredibly important to us.
+
+## License
+[Snap](http://github.com:intelsdi-x/snap), along with this plugin, is an Open Source software released under the Apache 2.0 [License](LICENSE).
+
+## Acknowledgements
+
+* Author: [Katarzyna Zabrocka](https://github.com/katarzyna-z)
---
+++ collector/collector.go
@@ -0,0 +1,335 @@
+// +build linux
+
+/*
+http://www.apache.org/licenses/LICENSE-2.0.txt
+
+
+Copyright 2016 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package collector
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/intelsdi-x/snap-plugin-utilities/config"
+	"github.com/intelsdi-x/snap/control/plugin"
+	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
+	"github.com/intelsdi-x/snap/core/serror"
+	"github.com/mitchellh/mapstructure"
+)
+
+const (
+	//vendor namespace part
+	vendor = "intel"
+
+	//pluginName namespace part
+	pluginName = "exec"
+
+	// version of plugin
+	version = 1
+
+	//pluginType type of plugin
+	pluginType = plugin.CollectorPluginType
+
+	//nsLength allowed length of namespace
+	nsLength = 3
+
+	//setFileConfigVar configuration variable to define path to setfile
+	setFileConfigVar = "setfile"
+
+	//execTimeOutConfigVar configuration variable to define max time for command/program execution
+	execTimeOutConfigVar = "execution_timeout"
+
+	//metricExecMapKey key in setfile to mark path to executable file
+	metricExecMapKey = "exec"
+
+	//metricTypeMapKey key in setfile to mark type of returned value of metric
+	metricTypeMapKey = "type"
+
+	//argsMapKey key in setfile to mark arguments needed by executable file
+	argsMapKey = "args"
+)
+
+//Plugin exec plugin struct which gathers plugin specific data
+type Plugin struct {
+	host    string
+	metrics map[string]metric
+	cmd     exeCmd
+}
+
+//Meta returns meta data for plugin
+func Meta() *plugin.PluginMeta {
+	return plugin.NewPluginMeta(
+		pluginName,
+		version,
+		pluginType,
+		[]string{},
+		[]string{plugin.SnapGOBContentType},
+		plugin.ConcurrencyCount(1))
+}
+
+// New creates instance of exec collector plugin
+func New() *Plugin {
+	metrics := make(map[string]metric)
+	host, err := os.Hostname()
+	if err != nil {
+		host = "localhost"
+	}
+	return &Plugin{host: host, metrics: metrics, cmd: executeCmd}
+}
+
+// GetMetricTypes returns list of available metric types
+// It returns error in case retrieval was not successful
+func (p *Plugin) GetMetricTypes(cfg plugin.PluginConfigType) ([]plugin.PluginMetricType, error) {
+	mts := []plugin.PluginMetricType{}
+	item, err := config.GetConfigItem(cfg, setFileConfigVar)
+	if err != nil {
+		return mts, err
+	}
+	setFilePath, ok := item.(string)
+	if !ok {
+		return mts, serror.New(fmt.Errorf("Incorrect type of configuration variable, cannot parse value of %s to string", setFileConfigVar), nil)
+	}
+
+	serr := p.getMetricsFromConfig(setFilePath)
+	if serr != nil {
+		log.WithFields(serr.Fields()).Error(serr.Error())
+		return mts, serr
+	}
+
+	for mtsName := range p.metrics {
+		mts = append(mts, plugin.PluginMetricType{
+			Namespace_: []string{vendor, pluginName, mtsName}})
+	}
+
+	return mts, nil
+}
+
+// CollectMetrics returns list of requested metric values
+// It returns error in case retrieval was not successful
+func (p *Plugin) CollectMetrics(metrics []plugin.PluginMetricType) ([]plugin.PluginMetricType, error) {
+	mts := []plugin.PluginMetricType{}
+	items, err := config.GetConfigItems(metrics[0], []string{setFileConfigVar, execTimeOutConfigVar})
+	if err != nil {
+		return nil, err
+	}
+	setFilePath, ok := items[setFileConfigVar].(string)
+	if !ok {
+		return mts, serror.New(fmt.Errorf("Incorrect type of configuration variable, cannot parse value of %s to string", setFileConfigVar), nil)
+	}
+
+	execTimeout, ok := items[execTimeOutConfigVar].(int)
+	if !ok {
+		return mts, serror.New(fmt.Errorf("Incorrect type of configuration variable, cannot parse value of %s to int", execTimeOutConfigVar), nil)
+	}
+	execTimeoutSec := time.Second * time.Duration(execTimeout)
+
+	serr := p.getMetricsFromConfig(setFilePath)
+	if serr != nil {
+		log.WithFields(serr.Fields()).Error(serr.Error())
+		return nil, serr
+	}
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	wg.Add(len(metrics))
+
+	for _, m := range metrics {
+
+		go func(m plugin.PluginMetricType) {
+			defer wg.Done()
+			logFields := map[string]interface{}{}
+
+			ns := m.Namespace()
+			logFields["namespace"] = "/" + strings.Join(ns, "/")
+			if len(ns) != nsLength {
+				serr := serror.New(fmt.Errorf("Incorrect namespace length"), logFields)
+				log.WithFields(serr.Fields()).Warn(serr.Error())
+				return
+			}
+			//get metric name, it is the last element of namespace
+			mtName := ns[nsLength-1]
+
+			timer := time.Now()
+			//execute command
+			cmdOut, serr := p.cmd(p.metrics[mtName].Exec, p.metrics[mtName].Args)
+			if serr != nil {
+				serr.SetFields(logFields)
+				log.WithFields(serr.Fields()).Warn(serr.Error())
+				return
+			}
+
+			if time.Since(timer) > execTimeoutSec {
+				//only notify that execution of command needs more time
+				serr := serror.New(fmt.Errorf("Waiting for output more than %d seconds", execTimeoutSec), logFields)
+				log.WithFields(serr.Fields()).Warn(serr.Error())
+			}
+
+			//convert output of command execution to type defined in setfile
+			data, serr := convertMetricType(cmdOut, p.metrics[mtName].Type)
+			if serr != nil {
+				serr.SetFields(logFields)
+				log.WithFields(serr.Fields()).Warn(serr.Error())
+				return
+			}
+
+			mt := plugin.PluginMetricType{
+				Namespace_: m.Namespace(),
+				Data_:      data,
+				Source_:    p.host,
+				Timestamp_: time.Now(),
+			}
+
+			mu.Lock()
+			mts = append(mts, mt)
+			mu.Unlock()
+
+		}(m)
+
+	}
+	wg.Wait()
+	return mts, nil
+}
+
+// GetConfigPolicy returns config policy
+// It returns error in case retrieval was not successful
+func (p *Plugin) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
+	cp := cpolicy.New()
+	config := cpolicy.NewPolicyNode()
+
+	r1, err := cpolicy.NewStringRule(setFileConfigVar, true)
+	if err != nil {
+		return cp, err
+	}
+	r1.Description = "Configuration file"
+	config.Add(r1)
+
+	r2, err := cpolicy.NewIntegerRule(execTimeOutConfigVar, false, 10)
+	if err != nil {
+		return cp, err
+	}
+	r2.Description = "Execution timeout"
+	config.Add(r2)
+
+	return cp, nil
+}
+
+// getMetricsFromConfig extracts metrics configuration from setfile
+func (p *Plugin) getMetricsFromConfig(setFilePath string) serror.SnapError {
+	logFields := map[string]interface{}{}
+	logFields["setFilePath"] = setFilePath
+
+	setFileContent, err := ioutil.ReadFile(setFilePath)
+	logFields["setFileContent"] = setFileContent
+	if err != nil {
+		return serror.New(err, logFields)
+	}
+
+	if len(setFileContent) == 0 {
+		return serror.New(fmt.Errorf("Settings file is empty"), logFields)
+	}
+
+	var setFileUnmarshalled map[string]interface{}
+	err = json.Unmarshal(setFileContent, &setFileUnmarshalled)
+	if err != nil {
+		return serror.New(fmt.Errorf("Settings file cannot be unmarshalled"), logFields)
+	}
+
+	err = mapstructure.Decode(setFileUnmarshalled, &p.metrics)
+	if err != nil {
+		return serror.New(fmt.Errorf("Settings file cannot be decoded"), logFields)
+	}
+
+	//validate if structure contains necessary fields
+	for k, m := range p.metrics {
+		if m.Type == "" {
+			return serror.New(fmt.Errorf("Incorrect structure of settings file, missing metric type for %s .", k), logFields)
+		}
+		if m.Exec == "" {
+			return serror.New(fmt.Errorf("Incorrect structure of settings file, missing metric exec for %s .", k), logFields)
+		}
+	}
+
+	return nil
+}
+
+//convertMetricType converts metric value to type defined in setfile
+func convertMetricType(data []byte, dataType string) (interface{}, serror.SnapError) {
+	var err error
+	var converted interface{}
+
+	dataStr := string(data)
+	logFields := map[string]interface{}{"date": data, "dataType": dataType}
+
+	switch dataType {
+	case "float64":
+		converted, err = strconv.ParseFloat(dataStr, 64)
+	case "float32":
+		converted, err = strconv.ParseFloat(dataStr, 32)
+	case "int64":
+		converted, err = strconv.ParseInt(dataStr, 10, 64)
+	case "int32":
+		converted, err = strconv.ParseInt(dataStr, 10, 32)
+	case "int16":
+		converted, err = strconv.ParseInt(dataStr, 10, 16)
+	case "int8":
+		converted, err = strconv.ParseInt(dataStr, 10, 8)
+	case "uint64":
+		converted, err = strconv.ParseUint(dataStr, 10, 64)
+	case "uint32":
+		converted, err = strconv.ParseInt(dataStr, 10, 32)
+	case "uint16":
+		converted, err = strconv.ParseInt(dataStr, 10, 16)
+	case "uint8":
+		converted, err = strconv.ParseInt(dataStr, 10, 16)
+	case "string":
+		converted = dataStr
+	default:
+		converted = dataStr
+		serr := serror.New(fmt.Errorf("Unsupported data type, metric saved as string"), logFields)
+		log.WithFields(serr.Fields()).Warn(serr.Error())
+	}
+
+	if err != nil {
+		return converted, serror.New(err, logFields)
+	}
+	return converted, nil
+}
+
+type exeCmd func(executableFilePath string, args []string) ([]byte, serror.SnapError)
+
+func executeCmd(executableFilePath string, args []string) ([]byte, serror.SnapError) {
+	cmdOut, err := exec.Command(executableFilePath, args...).Output()
+	if err != nil {
+		return cmdOut, serror.New(err)
+	}
+	return cmdOut, nil
+}
+
+type metric struct {
+	Exec string
+	Type string
+	Args []string
+}
