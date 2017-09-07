9be7c2aaf5a44e53bcdedfd4723630fdf8e25a21
katarzyna-z <katarzyna.kujawa@intel.com>
2017-09-07 13:56:29 +0200

    Update from pluginsync 0.1.14

--- scripts/large.sh
+++ scripts/large.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# File managed by pluginsync
 
 set -e
 set -u
@@ -6,31 +7,32 @@ set -o pipefail
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __proj_dir="$(dirname "$__dir")"
-__proj_name="$(basename $__proj_dir)"
+__proj_name="$(basename "$__proj_dir")"
 
+# shellcheck source=scripts/common.sh
 . "${__dir}/common.sh"
 
-# NOTE: these variables control the docker-compose image.
-export PLUGIN_SRC="${__proj_dir}"
-export LOG_LEVEL="${LOG_LEVEL:-"7"}"
-export PROJECT_NAME="${__proj_name}"
-
-TEST_TYPE="${TEST_TYPE:-"large"}"
-
-docker_folder="${__proj_dir}/scripts/docker/${TEST_TYPE}"
-
-_docker_project () {
-  echo ${docker_folder}
-  cd "${docker_folder}" && "$@"
+_verify_docker() {
+  type -p docker > /dev/null 2>&1 || _error "docker needs to be installed"
+  type -p docker-compose > /dev/null 2>&1 || _error "docker-compose needs to be installed"
+  docker version >/dev/null 2>&1 || _error "docker needs to be configured/running"
 }
 
-_debug "building docker compose images"
-_docker_project docker-compose build
-_debug "running test: ${TEST_TYPE}"
-_docker_project docker-compose up
-test_res=`docker-compose ps -q | xargs docker inspect -f '{{ .Name }} exited with status {{ .State.ExitCode }}' | awk '{print $5}'`
-echo "exit code from large_jenkins $test_res"
-_debug "stopping docker compose images"
-_docker_project docker-compose stop
-_docker_project docker-compose rm -f
-exit $test_res
+_verify_docker
+
+[[ -f "${__proj_dir}/build/linux/x86_64/${__proj_name}" ]] || (cd "${__proj_dir}" && make)
+
+SNAP_VERSION=${SNAP_VERSION:-"latest"}
+OS=${OS:-"alpine"}
+PLUGIN_PATH=${PLUGIN_PATH:-"${__proj_dir}"}
+DEMO=${DEMO:-"false"}
+TASK=${TASK:-""}
+
+if [[ ${DEBUG:-} == "true" ]]; then
+  cmd="cd /plugin/scripts && rescue rspec ./test/*_spec.rb"
+else
+  cmd="cd /plugin/scripts && rspec ./test/*_spec.rb"
+fi
+
+_info "running large test"
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${__proj_dir}":/plugin -e DEMO="${DEMO}" -e TASK="${TASK}" -e PLUGIN_PATH="${PLUGIN_PATH}" -e SNAP_VERSION="${SNAP_VERSION}" -e OS="${OS}" -ti intelsdi/serverspec:alpine /bin/sh -c "${cmd}"
